511468008955b79bd046c2fc4a42714d1134aafd
dop3ch3f <ifeanyi.ibekie@gmail.com>
2018-05-14 00:13:30 +0100

    FE part working with full integration with clarifai

--- facerecognition/src/App.js
+++ facerecognition/src/App.js
@@ -32,8 +32,26 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {}
     }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputimage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({ box: box });
   }
 
   onInputChange = (event) => {
@@ -42,15 +60,9 @@ class App extends Component {
 
   onSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
-      function (res) {
-        console.log(res.outputs[0].data.regions[0].region_info.bounding_box);
-      },
-      function (err) {
-        console.log(err);
-      }
-    );
-    console.log('clicked');
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then(res => this.displayFaceBox(this.calculateFaceLocation(res)))
+      .catch(err => console.log(err))
   }
 
   render() {
@@ -65,7 +77,7 @@ class App extends Component {
         <Rank />
         <ImageLinkForm onInputChange={this.onInputChange}
           onButtonClicked={this.onSubmit} />
-        <FaceRecognition imageUrl={this.state.imageUrl} />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
     );
   }
--- facerecognition/src/components/FaceRecognition/FaceRecognition.js
+++ facerecognition/src/components/FaceRecognition/FaceRecognition.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import './FaceRecognition.css';
 
 export default class FaceRecognition extends Component {
     //eslint-disable-next-line
@@ -8,12 +8,20 @@ export default class FaceRecognition extends Component {
     }
 
     render() {
-        const { imageUrl } = this.props;
+        const { imageUrl, box } = this.props;
         return (
             <div className='center ma'>
                 <div className='absolute mt2 '>
-                    <img src={imageUrl} alt='Result'
+                    <img id='inputimage' src={imageUrl} alt='Result'
                         width='500px' height='auto' />
+                    <div className='bounding-box'
+                        style={{
+                            top: box.topRow,
+                            right: box.rightCol,
+                            left: box.leftCol,
+                            bottom: box.bottomRow
+                        }}
+                    ></div>
                 </div>
             </div>
         )
